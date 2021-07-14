ed8d93e3d8e975302290f27bc28a837624140a77
Emily F <emily@emilyfoxmusic.co.uk>
2021-07-14 23:12:59 +0100

    refactor: use response route for 'get' endpoints

--- lambdas/websockets/listGet.js
+++ lambdas/websockets/listGet.js
@@ -1,34 +1,19 @@
-const Responses = require('../common/API_Responses');
-const Dynamo = require('../common/Dynamo');
-const WebSocket = require('../common/websocketMessage');
+const Responses = require("../common/API_Responses");
+const Dynamo = require("../common/Dynamo");
 
-const usersTableName = process.env.usersTableName;
 const songListTableName = process.env.songListTableName;
 
-exports.handler = async event => {
-    console.info('Queue get request received', event);
-
-    const { connectionId } = event.requestContext;
-    const userRecord = await Dynamo.get({ ID: connectionId }, usersTableName);
-    const { domainName, stage } = userRecord;
-
-    const songItems = await Dynamo.getAll(songListTableName);
-
-    await WebSocket.send({
-        domainName,
-        stage,
-        connectionId,
-        data: {
-            action: 'listGet',
-            data: songItems.map((item) => ({
-                id: item.ID,
-                title: item.Title,
-                artist: item.Artist,
-                numberOfPlays: item.NumberOfPlays,
-                lastPlayed: item.LastPlayed ? item.LastPlayed : null
-            }))
-        },
-    });
-
-    return Responses._200({ message: 'successfully sent songs'});
+exports.handler = async (event) => {
+  console.info("Queue get request received", event);
+  const songItems = await Dynamo.getAll(songListTableName);
+  return Responses._200({
+    action: "listGet",
+    data: songItems.map((item) => ({
+      id: item.ID,
+      title: item.Title,
+      artist: item.Artist,
+      numberOfPlays: item.NumberOfPlays,
+      lastPlayed: item.LastPlayed ? item.LastPlayed : null,
+    })),
+  });
 };
