eaee4cd425b65bcb97dbd824debf1e330f8863a3
Emily F <emily@emilyfoxmusic.co.uk>
2021-07-26 13:25:07 +0100

    feat: properly implement auth endpoint with token verification and admin detection

--- lambdas/http/auth.js
+++ lambdas/http/auth.js
@@ -1,13 +1,16 @@
-const https = require('https');
+const axios = require('axios');
+const jwt = require('jsonwebtoken');
+var jwksClient = require('jwks-rsa');
 const response = require('../helpers/apiResponses');
 const validation = require('../helpers/parameterValidation');
 const ssm = require('../helpers/ssm');
 
 const twitchClientId = process.env.twitchClientId;
 const twitchRedirectUri = process.env.twitchRedirectUri;
+const adminUsers = process.env.adminUsers.split(',');
 
 exports.handler = async (event) => {
-  console.log('Authentication request received:', event);
+  console.info('Authentication request received:', event);
   var code = event.queryStringParameters && event.queryStringParameters.code;
 
   if (!validation.isRequiredString(code)) {
@@ -17,24 +20,37 @@ exports.handler = async (event) => {
   const twitchClientSecret = await ssm.getDecryptedParameter(
     'TwitchClientSecret'
   );
-  const path = `/oauth2/token?client_id=${twitchClientId}&client_secret=${twitchClientSecret}&code=${code}&grant_type=authorization_code&redirect_uri=${twitchRedirectUri}`;
-  const options = {
-    hostname: 'id.twitch.tv',
-    path,
-    method: 'POST',
-  };
 
   console.info('Fetching token from Twitch');
+  const authenticationResponse = await axios.post(
+    `https://id.twitch.tv/oauth2/token?client_id=${twitchClientId}&client_secret=${twitchClientSecret}&code=${code}&grant_type=authorization_code&redirect_uri=${twitchRedirectUri}`
+  );
+  const idToken = authenticationResponse.data.id_token;
+
+  function getSigningKey(header, callback) {
+    jwksClient({
+      jwksUri: 'https://id.twitch.tv/oauth2/keys',
+    }).getSigningKey(header.kid, function (_, key) {
+      var signingKey = key.publicKey || key.rsaPublicKey;
+      callback(null, signingKey);
+    });
+  }
+
   return new Promise((resolve, reject) => {
-    const tokenRequest = https.request(options, (res) => {
-      res.setEncoding('utf8');
-      res.on('data', (chunk) => {
-        resolve({ token: JSON.parse(chunk).id_token, isAdmin: true });
-      });
-    });
-    tokenRequest.on('error', (e) => {
-      reject(e);
-    });
-    tokenRequest.end();
+    jwt.verify(
+      idToken,
+      getSigningKey,
+      { audience: twitchClientId, issuer: 'https://id.twitch.tv/oauth2' },
+      function (err, decoded) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({
+            token: idToken,
+            isAdmin: adminUsers.includes(decoded.preferred_username),
+          });
+        }
+      }
+    );
   });
 };
